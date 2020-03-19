b11fdfb0c252607dab86b6844199b5f90a81c4e7
Bogdan Aksanov <softwase@gmail.com>
2020-03-20 01:43:07 +0200

    fixed exist functions, add new function

--- app.js
+++ app.js
@@ -4,33 +4,30 @@
         if (typeof str !== 'string') {
             return;
         }
-
+    
         if (!str) {
             return true;
         }
-
+       
         const map = {
             ')': '(',
             '}': '{',
             ']': '['
         };
 
-        const strArr = str.replace(/\s/g, '').split('');
         let brackets = [];
-
-        strArr.forEach((char) => {
-            if (Object.values(map).includes(char)) {
-                brackets.push(char);
-            } else if (Object.keys(map).includes(char)) {
-                if (brackets.length && map[char] !== brackets[brackets.length - 1]){
-                    return false;
-                }
     
-                brackets.pop();
+        for (let i = 0; i < str.length; i++) {
+            if (str[i] === '(' || str[i] === '{' || str[i] === '[') {
+                brackets.push(str[i]);
+            } else if (str[i] === ')' || str[i] === '}' || str[i] === ']'){
+                if (!brackets.length || map[str[i]] !== brackets.pop()) {
+                    return false;
+                }          
             }
-        })
-
-        return !brackets.length;
+        }
+    
+        return !brackets.length;    
     }
 
     let str = '([1231    123 ()   23123])';
@@ -38,14 +35,18 @@
     let str2 = '([) ()]';
     let str3 = '';
     let str4 = [];
+    let str5 = '{}{}}}';
+    let str6 = '}}}{{{';
 
     console.log(isCorrectNested(str));  //true
     console.log(isCorrectNested(str1)); //true
     console.log(isCorrectNested(str2)); //false
     console.log(isCorrectNested(str3)); //true
     console.log(isCorrectNested(str4)); //undefined
+    console.log(isCorrectNested(str5)); //false
+    console.log(isCorrectNested(str6)); //false
 }
-/* --------------------------------------------------------- */
+console.log('/* --------------------------------------------------------- */')
 {
     function isNotValidNumber(number) {
         return Number.isNaN(+number) || Array.isArray(number) || !number || number < 0 || number > 200000;
@@ -53,17 +54,17 @@
 
     function isInteger(number) {
         if (isNotValidNumber(number)) {
-            return new Error('Not valid number');
+            return;
         }
 
         return +number === parseInt(number);
     };
 
-    const isInteger1 = (number) => isNotValidNumber(number) ? new Error('Not valid number') : ~~+number === +number
+    const isInteger1 = (number) => isNotValidNumber(number) ? undefined : ~~+number === +number
 
     function isInteger2(number) {
         if (isNotValidNumber(number)) {
-            return new Error('Not valid number');
+            return;
         }
 
         return (+number ^ 0) === +number;
@@ -71,12 +72,12 @@
 
 
 
-    console.log(isInteger(200001));     //Error: Not valid number
+    console.log(isInteger(200001));     //undefined
     console.log(isInteger(5.5));        // false
-    console.log(isInteger('asadsa'));   //Error: Not valid number
+    console.log(isInteger('asadsa'));   //undefined
     console.log(isInteger(5));          //true
 
-    console.log(isInteger1([]));        //Error: Not valid number
+    console.log(isInteger1([]));        //undefined
     console.log(isInteger1(5.5));       //false
     console.log(isInteger1('3'));       //true
 
@@ -84,7 +85,7 @@
     console.log(isInteger2(5.5));       //false
     console.log(isInteger2('3'));       //true
 }
-/* --------------------------------------------------------- */
+console.log('/* --------------------------------------------------------- */')
 {
     function isOnlyBrackets(str) {
         if (typeof str !== 'string') {
@@ -94,13 +95,33 @@
         return new RegExp(/^[\(\{\[\]\}\)\/]*$/).test(str);
     }
 
+    function isOnlyBracketsLoop(str) {
+        if (typeof str !== 'string') {
+            return;
+        }
+
+        for (let i = 0; i < str.length; i++) {
+            if (!(str[i] === '(' || str[i] === '{' || str[i] === '[' ||
+                str[i] === ')' || str[i] === '}' || str[i] === ']' || str[i] === '/')
+            ) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     // const isOnlyBrackets = str => ( new RegExp(/^[\(\{\[\]\}\)\/]*$/).test(str) );
 
-    let str = '(((((({[}}}}{{{{{}}{{{{}{{///////}}}}}}}}]}/)';
+    let str = '(((((({/)))]]]][[[[{{{}]{}{}{]///';
     let str1 = '((({{}}()23//)';
     let arr = [];
 
     console.log(isOnlyBrackets(str));   //true
     console.log(isOnlyBrackets(str1));  //false
     console.log(isOnlyBrackets(arr));   //undefined
+    
+    console.log(isOnlyBracketsLoop(str));   //true
+    console.log(isOnlyBracketsLoop(str1));  //false
+    console.log(isOnlyBracketsLoop(arr));   //undefined
 }
